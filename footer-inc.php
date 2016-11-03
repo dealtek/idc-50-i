2c1d12609c874fb3e239c040caf6f5df0e5505d7
dealtek <dealtek@gmail.com>
2016-11-03 16:53:06 -0700

    v1

---
+++ footer-inc.php
@@ -0,0 +1,22 @@
+
+<style type="text/css">
+.green1a {
+	
+	/*font-family: Verdana, Geneva, sans-serif;
+	*/
+	font-weight: bold;
+	color: #0C0;
+}
+</style>
+
+<div data-role="controlgroup" data-type="horizontal" class="center1">
+
+<a href="dashboard.php" rel="external" data-role="button" class="ui-btn ui-shadow ui-corner-all ui-btn-icon-left ui-icon-home" style="text-align: left;">Home</a>
+
+<a href="invoices-list.php" rel="external" data-role="button" class="ui-btn ui-shadow ui-btn-icon-left" style="text-align: center;"><span class="green1a"> $ </span> Pay</a>
+
+<a href="client-contact-info.php" rel="external" data-role="button" class="ui-btn ui-shadow ui-btn-icon-right ui-icon-info" style="text-align: center;">Info</a>
+
+<a href="collection-sites.php" rel="external" class="ui-btn ui-shadow ui-corner-all ui-btn-icon-right ui-icon-search" style="text-align: right;">Sites</a>
+
+</div>
